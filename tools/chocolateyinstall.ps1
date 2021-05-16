4954eb5d2ed8413fc4a1056be376e5daa1432718
deepu105 <d4udts@gmail.com>
2021-05-16 14:06:05 +0200

    update choco specs

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,20 +1,24 @@
 $ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/kdash-rs/kdash/releases/download/v0.0.6/kdash-windows.tar.gz'
-$url64      = 'https://github.com/kdash-rs/kdash/releases/download/v0.0.6/kdash-windows.tar.gz'
+
+$PackageName = 'kdash'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64       = 'https://github.com/kdash-rs/kdash/releases/download/v0.0.9/kdash-windows.tar.gz'
+$checksum64  = 'dad482292bddffde400f97d5a0e3d500d13afbc8ea3ef53f9d9629c8bfa9e37a'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'kdash'
+  packageName   = $packageName
+  softwareName  = $packageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = $url
+  url           = $url64
   url64bit      = $url64
 
-  checksum      = '6765dfc966e8c9bbbe4e6a2fa7c2f5b58b06ac1a2ba3db404aaa95b198feb292'
+  checksum      = $checksum64
   checksumType  = 'sha256'
-  checksum64    = '6765dfc966e8c9bbbe4e6a2fa7c2f5b58b06ac1a2ba3db404aaa95b198feb292'
+  checksum64    = $checksum64
   checksumType64= 'sha256'
 
 }
 Install-ChocolateyZipPackage @packageArgs
+$File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.tar
+Get-ChocolateyUnzip -fileFullPath $File.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
