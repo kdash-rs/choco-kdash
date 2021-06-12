4cb207d0b23cbaa9b14a804577ff24f54efd2a10
deepu105 <d4udts@gmail.com>
2021-06-12 13:50:15 +0000

    Update package for KDash release v0.1.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop';
 
 $PackageName = 'kdash'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://github.com/kdash-rs/kdash/releases/download/v0.0.9/kdash-windows.tar.gz'
-$checksum64  = 'dad482292bddffde400f97d5a0e3d500d13afbc8ea3ef53f9d9629c8bfa9e37a'
+$url64       = 'https://github.com/kdash-rs/kdash/releases/download/v0.1.2/kdash-windows.tar.gz'
+$checksum64  = 'd29e55f7a36c1d175434b80104f50f250bd4f7140ab0c5861f8183f6fa8179d0'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -11,8 +11,13 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url64
+  url64bit      = $url64
+
   checksum      = $checksum64
   checksumType  = 'sha256'
+  checksum64    = $checksum64
+  checksumType64= 'sha256'
+
 }
 Install-ChocolateyZipPackage @packageArgs
 $File = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.tar
