0895b1b4c95ac5b244cb904782878f42ad0073bc
deepu105 <d4udts@gmail.com>
2026-06-29 12:24:09 +0000

    Update package for KDash release v2.0.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop';
 
 $PackageName = 'kdash'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://github.com/kdash-rs/kdash/releases/download/v2.0.1/kdash-windows.tar.gz'
-$checksum64  = 'df837f41d863404355ac285ce8d909d62575d867b41fcacae3f4a81ec0472583'
+$url64       = 'https://github.com/kdash-rs/kdash/releases/download/v2.0.2/kdash-windows.tar.gz'
+$checksum64  = '712cf2687786a2f395b1e103cb0c1be363f9ad9bae551a8ebdf135b7ee2f2efc'
 
 $packageArgs = @{
   packageName   = $packageName
